0b24e15c1bcd9794b40f78d499396f818e944c3c
Richard Hofman <r.hofman@zaict.nl>
2025-03-05 08:49:58 +0100

    Initial commit: SharePoint Framework declaratieformulier project

---
+++ src/webparts/declaratieformulier/components/IDeclaratieformulierProps.ts
@@ -0,0 +1,7 @@
+export interface IDeclaratieformulierProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/declaratieformulier/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IDeclaratieformulierWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'DeclaratieformulierWebPartStrings' {
+  const strings: IDeclaratieformulierWebPartStrings;
+  export = strings;
+}
